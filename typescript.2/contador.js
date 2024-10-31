f881036ea5fc3631ea5354a625ccb97dfb0b1b3b
larucha <134147995+larucha@users.noreply.github.com>
2024-10-31 09:33:20 -0300

    hola

--- typescript.2/contador.js
+++ typescript.2/contador.js
@@ -2,37 +2,36 @@
 var tribunegra = { punto: 0 };
 var triburoja = { punto: 0 };
 // Obtener los elementos de entrada de puntos
-var handball_input_tribunegra = document.getElementById("handball-a");
-var resistencia_input_tribunegra = document.getElementById("resistencia-a");
-var ajedrez_input_tribunegra = document.getElementById("Ajedrez-a");
-var handball_input_triburoja = document.getElementById("handball-b");
-var resistencia_input_triburoja = document.getElementById("resistencia-b");
-var ajedrez_input_triburoja = document.getElementById("Ajedrez-b");
+var Handball_input_tribunegra = document.getElementById("Handball-a");
+var Resistencia_input_tribunegra = document.getElementById("Resistencia-a");
+var Ajedrez_input_tribunegra = document.getElementById("Ajedrez-a");
+var Handball_input_triburoja = document.getElementById("Handball-b");
+var Resistencia_input_triburoja = document.getElementById("Resistencia-b");
+var Ajedrez_input_triburoja = document.getElementById("Ajedrez-b");
 // Elementos de salida
-var Outputpuntostribunegra = document.getElementById('resultado');
-var Outputpuntostriburoja = document.getElementById("puntos tribu roja");
+var resultadosOutput = document.getElementById('resultados');
 function Agregarpuntos(equipo) {
     if (equipo === 'tribu negra') {
-        tribunegra.punto += parseInt(handball_input_tribunegra.value, 10) || 0;
-        tribunegra.punto += parseInt(resistencia_input_tribunegra.value, 10) || 0;
-        tribunegra.punto += parseInt(ajedrez_input_tribunegra.value, 10) || 0;
+        tribunegra.punto += parseInt(Handball_input_tribunegra.value, 10) || 0;
+        tribunegra.punto += parseInt(Resistencia_input_tribunegra.value, 10) || 0;
+        tribunegra.punto += parseInt(Ajedrez_input_tribunegra.value, 10) || 0;
     }
     else if (equipo === 'tribu roja') {
-        triburoja.punto += parseInt(handball_input_triburoja.value, 10) || 0;
-        triburoja.punto += parseInt(resistencia_input_triburoja.value, 10) || 0;
-        triburoja.punto += parseInt(ajedrez_input_triburoja.value, 10) || 0;
+        triburoja.punto += parseInt(Handball_input_triburoja.value, 10) || 0;
+        triburoja.punto += parseInt(Resistencia_input_triburoja.value, 10) || 0;
+        triburoja.punto += parseInt(Ajedrez_input_triburoja.value, 10) || 0;
     }
 }
 function mostrarResultados() {
-    var resultado = '';
+    var resultados = '';
     if (tribunegra.punto > triburoja.punto) {
-        resultado = 'La tribu negra es la ganadora con' + tribunegra.punto + 'puntos';
+        resultados = 'La tribu negra es la ganadora con ' + tribunegra.punto + 'puntos';
     }
     else if (tribunegra.punto < triburoja.punto) {
-        resultado = 'La tribu negra es la ganadora con' + triburoja.punto + 'puntos';
+        resultados = 'La tribu negra es la ganadora con' + triburoja.punto + 'puntos';
     }
     else {
-        resultado = 'Ambos equipos están empatados con' + tribunegra.punto + 'puntos';
+        resultados = 'Ambos equipos están empatados con' + tribunegra.punto + 'puntos';
     }
-    Outputpuntostribunegra.innerText = resultado;
+    resultadosOutput.innerText = resultados;
 }
--- typescript.2/contador.ts
+++ typescript.2/contador.ts
@@ -7,40 +7,40 @@ let triburoja = { punto: 0 };
 
 
 // Obtener los elementos de entrada de puntos
-const handball_input_tribunegra = document.getElementById("handball-a") as HTMLInputElement;
-const resistencia_input_tribunegra = document.getElementById("resistencia-a") as HTMLInputElement;
-const ajedrez_input_tribunegra = document.getElementById("Ajedrez-a") as HTMLInputElement;
+const Handball_input_tribunegra = document.getElementById("Handball-a") as HTMLInputElement;
+const Resistencia_input_tribunegra = document.getElementById("Resistencia-a") as HTMLInputElement;
+const Ajedrez_input_tribunegra = document.getElementById("Ajedrez-a") as HTMLInputElement;
 
-const handball_input_triburoja = document.getElementById("handball-b") as HTMLInputElement;
-const resistencia_input_triburoja = document.getElementById("resistencia-b") as HTMLInputElement;
-const ajedrez_input_triburoja = document.getElementById("Ajedrez-b") as HTMLInputElement;
+const Handball_input_triburoja = document.getElementById("Handball-b") as HTMLInputElement;
+const Resistencia_input_triburoja = document.getElementById("Resistencia-b") as HTMLInputElement;
+const Ajedrez_input_triburoja = document.getElementById("Ajedrez-b") as HTMLInputElement;
 
 // Elementos de salida
-const Outputpuntostribunegra = document.getElementById('resultado') as HTMLElement;
-const Outputpuntostriburoja = document.getElementById("puntos tribu roja") as HTMLElement;
+const resultadosOutput = document.getElementById('resultados') as HTMLOutputElement;
 
-function Agregarpuntos(equipo:string): void {
+
+function Agregarpuntos(equipo:string) {
     if(equipo ==='tribu negra'){
-    tribunegra.punto += parseInt(handball_input_tribunegra.value, 10) || 0;
-    tribunegra.punto += parseInt(resistencia_input_tribunegra.value, 10) || 0;
-    tribunegra.punto += parseInt(ajedrez_input_tribunegra.value, 10) || 0;
+    tribunegra.punto += parseInt(Handball_input_tribunegra.value, 10) || 0;
+    tribunegra.punto += parseInt(Resistencia_input_tribunegra.value, 10) || 0;
+    tribunegra.punto += parseInt(Ajedrez_input_tribunegra.value, 10) || 0;
     }else if (equipo === 'tribu roja'){
-    triburoja.punto += parseInt(handball_input_triburoja.value, 10) || 0;
-    triburoja.punto += parseInt(resistencia_input_triburoja.value, 10) || 0;
-    triburoja.punto += parseInt(ajedrez_input_triburoja.value, 10) || 0;
+    triburoja.punto += parseInt(Handball_input_triburoja.value, 10) || 0;
+    triburoja.punto += parseInt(Resistencia_input_triburoja.value, 10) || 0;
+    triburoja.punto += parseInt(Ajedrez_input_triburoja.value, 10) || 0;
     }
 }
 
 function mostrarResultados() {
-    let resultado ='';
-    if(tribunegra.punto > triburoja.punto) {
-        resultado='La tribu negra es la ganadora con' + tribunegra.punto + 'puntos';
+    let resultados ='';
+    if (tribunegra.punto > triburoja.punto) {
+        resultados= 'La tribu negra es la ganadora con ' +  tribunegra.punto  + 'puntos';
     }else if (tribunegra.punto < triburoja.punto) {
-        resultado='La tribu negra es la ganadora con' + triburoja.punto + 'puntos';
+        resultados='La tribu negra es la ganadora con' + triburoja.punto + 'puntos';
     }else {
-        resultado = 'Ambos equipos están empatados con' + tribunegra.punto + 'puntos';
+        resultados = 'Ambos equipos están empatados con' + tribunegra.punto + 'puntos';
     }
     
-    Outputpuntostribunegra.innerText = resultado;
+    resultadosOutput.innerText = resultados;
     
 }
